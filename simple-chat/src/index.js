734b9cbb44031964483d745a7b67f324b401936e
Ulugbek Istamov <istamovulugbek@yandex.ru>
2024-10-04 16:10:51 -0700

    fixed padding and added new styles with compose variables color

--- simple-chat/src/index.js
+++ simple-chat/src/index.js
@@ -2,12 +2,13 @@ import './index.css';
 const form = document.querySelector('form');
 const input = document.querySelector('.form-input');
 const messageList = document.querySelector('.message-list');
+const sendButtonIcon = document.querySelector('.send-button span');
 let messages = JSON.parse(localStorage.getItem('messages')) || [];
 
 window.addEventListener('load', loadMessages);
 
 form.addEventListener('submit', handleSubmit);
-
+input.addEventListener('input', toggleSendButton);
 form.addEventListener('keypress', handleKeyPress);
 
 function handleSubmit(event) {
@@ -26,6 +27,7 @@ function handleSubmit(event) {
         
         displayMessage(message);
         input.value = '';
+        toggleSendButton();
         scrollToBottom();
     }
 }
@@ -45,11 +47,30 @@ function loadMessages() {
 function displayMessage(message) {
     const messageElement = document.createElement('div');
     messageElement.classList.add('message-from-user');
-    messageElement.innerHTML = `${message.text} <small>${message.time}</small>`;
+
+    const messageText = document.createElement('p');
+    messageText.textContent = message.text;
+
+    const messageTime = document.createElement('small');
+    messageTime.classList.add('small-time-message');
+    messageTime.textContent = message.time;
     
+    const messageCheck = document.createElement('span');
+    messageCheck.classList.add('material-icons');
+    messageCheck.classList.add('check');
+    messageCheck.textContent = 'check';
+
+    messageElement.appendChild(messageText);
+    messageElement.appendChild(messageTime);
+    messageElement.appendChild(messageCheck);
+
     messageList.appendChild(messageElement);
 }
 
 function scrollToBottom() {
     messageList.scrollTop = messageList.scrollHeight;
+}
+
+function toggleSendButton() {
+   input.value.trim() ? sendButtonIcon.textContent = 'send' : sendButtonIcon.textContent = 'attach_file';
 }
