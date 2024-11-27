298a836d5aba701a794d8fbdfe541a61f8fb1313
Ulugbek Istamov <istamovulugbek@yandex.ru>
2024-11-27 21:42:38 +0300

    fix styles and deploy with vite config

--- react-chat/vite.config.ts
+++ react-chat/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/2024-2-VK-EDU-Frontend-U-Istamov/pr/dz5/',
   plugins: [react()],
 })
