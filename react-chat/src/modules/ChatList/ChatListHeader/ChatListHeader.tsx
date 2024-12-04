d886b266453dbf150af31774479ec6ba5dc45f4b
Ulugbek Istamov <istamovulugbek@yandex.ru>
2024-12-05 02:43:26 +0300

    navigate to /profile

--- react-chat/src/modules/ChatList/ChatListHeader/ChatListHeader.tsx
+++ react-chat/src/modules/ChatList/ChatListHeader/ChatListHeader.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import styles from './ChatListHeader.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const ChatListHeader: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleViewProfile = () => {
+    navigate(`/profile`);
+  };
+
   return (
     <header className={styles['list-header']}>
       <button className={styles['header-tool']}>
-        <MenuIcon/>
+        <MenuIcon onClick={handleViewProfile}/>
       </button>
       <div className={styles['header-title']}>Messenger</div>
       <button className={styles['header-tool']}>
